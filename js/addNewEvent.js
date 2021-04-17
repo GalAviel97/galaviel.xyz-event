735d6295e2be60e8c3c5e35d50adae83bf5b68ca
GalAviel97 <gal.aviel97@icloud.com>
2021-04-17 16:02:11 +0300

    New repository for galaviel.xyz/event
    New repository for galaviel.xyz/event

---
+++ js/addNewEvent.js
@@ -0,0 +1,24 @@
+/* --------------------------
+* UPDATE UPCOMING EVENT DATE
+* -------------------------- 
+*
+* IMPORTANT: The date has to be in the Pacific Standard Time (PST) zone
+*
+*/
+const 
+// Format: YYYY (2020)
+year = 2021,
+// Format: MM (09) or M (9), both are valid 
+month = 4,
+// Format: DD (09) or D (9), both are valid
+day = 20  
+// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM 
+hour = 09,
+// Format: MM (09) or M (9), both are valid
+minute = 00;
+
+/* --------------------------
+* UPDATE UPCOMING EVENT NAME
+* --------------------------
+*/
+const eventName = 'Spring Loaded.';
