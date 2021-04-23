e10224154e382bac89d8d48c3c8cf445a3e67a65
GalAviel97 <60825369+GalAviel97@users.noreply.github.com>
2021-04-23 19:25:31 +0300

    WWDC21 Update

--- js/addNewEvent.js
+++ js/addNewEvent.js
@@ -9,9 +9,9 @@ const
 // Format: YYYY (2020)
 year = 2021,
 // Format: MM (09) or M (9), both are valid 
-month = 4,
+month = 6,
 // Format: DD (09) or D (9), both are valid
-day = 20  
+day = 7  
 // Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM 
 hour = 09,
 // Format: MM (09) or M (9), both are valid
@@ -21,4 +21,4 @@ minute = 00;
 * UPDATE UPCOMING EVENT NAME
 * --------------------------
 */
-const eventName = 'Spring Loaded.';
+const eventName = 'WWDC 2021';
